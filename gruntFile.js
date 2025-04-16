cf592f6870ba2f156a31bf1a76d2e6fa4515decd
swapnilgithubexe <swapnildutta2002@gmail.com>
2025-04-16 16:11:24 +0530

    implememted the cssmin task

--- gruntFile.js
+++ gruntFile.js
@@ -11,11 +11,23 @@ module.exports = function (grunt) {
           //for all js files use ["src/js/*.js"]
         }
       }
+    },
+    cssmin: {
+      target: {
+        files: [{
+          cwd: "src/css",
+          src: ["*.css", "!*.min.css"],
+          dest: "dest.css",
+          ext: ".min.css"
+        }]
+      }
     }
   })
   //load libraries
   grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
 
   //setting up tasks
   grunt.registerTask("default", ['uglify']);
+  grunt.registerTask("default", ['cssmin']);
 }
