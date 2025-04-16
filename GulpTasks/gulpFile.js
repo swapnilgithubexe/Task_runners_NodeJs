712568b5ca635e9b2a67205d87c61f6c60d51f57
swapnilgithubexe <swapnildutta2002@gmail.com>
2025-04-16 21:05:58 +0530

    using gulp

--- GulpTasks/gulpFile.js
+++ GulpTasks/gulpFile.js
@@ -1,6 +1,10 @@
 const gulp = require("gulp");
+import imagemin from "gulp-imagemin";
 
 gulp.task("imagemin", () => {
   //define task
+  gulp.src("src/images/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("dest/images"))
 
-})
+});
